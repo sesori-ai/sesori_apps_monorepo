994ec51c6fdbc8b0bc0cf078a469220a91e15301
Daniil Shumko <7705571+daniil-shumko@users.noreply.github.com>
2026-04-02 22:50:36 -0300

    lint fix

--- mobile/module_core/lib/sesori_dart_core.dart
+++ mobile/module_core/lib/sesori_dart_core.dart
@@ -44,9 +44,9 @@ export "src/cubits/notification_preferences/notification_preferences_cubit.dart"
 export "src/cubits/notification_preferences/notification_preferences_state.dart";
 export "src/cubits/project_list/project_list_cubit.dart";
 export "src/cubits/project_list/project_list_state.dart";
+export "src/cubits/session_detail/queued_session_submission.dart";
 export "src/cubits/session_detail/session_detail_cubit.dart";
 export "src/cubits/session_detail/session_detail_state.dart";
-export "src/cubits/session_detail/queued_session_submission.dart";
 export "src/cubits/session_diffs/diff_cubit.dart";
 export "src/cubits/session_diffs/diff_state.dart";
 export "src/cubits/session_list/session_list_cubit.dart";
