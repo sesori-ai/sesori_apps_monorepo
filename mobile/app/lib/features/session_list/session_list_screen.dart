7bbaa52b6483bed2b214d80f38acc49f97044388
Alexandru Dochioiu <alex@dochioiu.com>
2026-06-05 17:36:04 +0300

    refactor: split session list screen wrappers
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- mobile/app/lib/features/session_list/session_list_screen.dart
+++ mobile/app/lib/features/session_list/session_list_screen.dart
@@ -6,19 +6,17 @@ import "package:sesori_shared/sesori_shared.dart";
 import "package:theme_zyra/module_zyra.dart";
 
 import "../../core/constants.dart";
-import "../../core/di/injection.dart";
 import "../../core/extensions/build_context_x.dart";
 import "../../core/routing/app_router.dart";
-import "../../core/status_colors.dart";
 import "../../core/widgets/app_modal_bottom_sheet.dart";
 import "../../l10n/app_localizations.dart";
-import "pr_status_row.dart";
 import "rename_session_dialog.dart";
+import "session_list_cubit_provider.dart";
+import "session_list_scaffold.dart";
 
 part "session_list_actions.dart";
 part "session_cleanup_dialogs.dart";
 part "session_force_dialog.dart";
-part "session_list_widgets.dart";
 
 class SessionListScreen extends StatelessWidget {
   final String projectId;
@@ -32,32 +30,21 @@ class SessionListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => SessionListCubit(
-        sessionService: getIt<SessionService>(),
-        projectService: getIt<ProjectService>(),
-        connectionService: getIt<ConnectionService>(),
-        sseEventRepository: getIt<SseEventRepository>(),
-        routeSource: getIt<RouteSource>(),
+    return SessionListCubitProvider(
+      projectId: projectId,
+      child: _SessionListBody(
         projectId: projectId,
-        failureReporter: getIt<FailureReporter>(),
+        projectName: projectName,
       ),
-      child: _SessionListBody(projectName: projectName),
     );
   }
 }
 
 class _SessionListBody extends StatelessWidget {
+  final String projectId;
   final String? projectName;
 
-  const _SessionListBody({this.projectName});
-
-  void _goToNewSession({required BuildContext context}) {
-    final projectId = context.read<SessionListCubit>().projectId;
-    context.pushRoute(
-      AppRoute.newSession(projectId: projectId),
-    );
-  }
+  const _SessionListBody({required this.projectId, this.projectName});
 
   void _showSessionActions({required BuildContext context, required Session session}) {
     final loc = context.loc;
@@ -68,7 +55,7 @@ class _SessionListBody extends StatelessWidget {
       context: context,
       builder: (sheetContext) {
         return Column(
-          mainAxisSize: .min,
+          mainAxisSize: MainAxisSize.min,
           children: [
             ListTile(
               leading: const Icon(Icons.edit_outlined),
@@ -113,110 +100,29 @@ class _SessionListBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final loc = context.loc;
-    final state = context.watch<SessionListCubit>().state;
-    final title = switch (projectName) {
-      final name? => loc.sessionListTitleWithName(name),
-      null => loc.sessionListTitle,
-    };
-
-    final showArchived = state is SessionListLoaded && state.showArchived;
-    final baseBranch = state is SessionListLoaded ? state.baseBranch : null;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(title),
-            if (baseBranch != null)
-              Text(
-                baseBranch,
-                style: context.zyra.textTheme.textXs.regular.copyWith(
-                  color: context.zyra.colors.textSecondary,
-                ),
-              ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(showArchived ? Icons.archive : Icons.archive_outlined),
-            tooltip: loc.sessionListToggleArchived,
-            onPressed: state is SessionListLoaded ? () => context.read<SessionListCubit>().toggleArchived() : null,
+    return SessionListScaffold(
+      projectName: projectName,
+      onNewSession: () {
+        context.pushRoute(AppRoute.newSession(projectId: projectId));
+      },
+      onSessionTap: (session) {
+        context.pushRoute(
+          AppRoute.sessionDetail(
+            projectId: projectId,
+            sessionId: session.id,
+            sessionTitle: session.title ?? "",
+            readOnly: false,
           ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _goToNewSession(context: context),
-        tooltip: loc.sessionListNewSession,
-        child: const Icon(Icons.add),
-      ),
-      body: switch (state) {
-        SessionListLoading() => const Center(
-          child: CircularProgressIndicator(),
-        ),
-        SessionListLoaded(:final sessions, :final isRefreshing) => Column(
-          children: [
-            if (isRefreshing) const LinearProgressIndicator(),
-            Expanded(
-              child: RefreshIndicator(
-                onRefresh: () async {
-                  final success = await context.read<SessionListCubit>().refreshSessions(waitForPrData: true);
-                  if (!context.mounted) return;
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(success ? loc.sessionListRefreshSuccess : loc.sessionListRefreshFailed),
-                      duration: kSnackBarDuration,
-                    ),
-                  );
-                },
-                child: sessions.isEmpty
-                    ? CustomScrollView(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        slivers: [
-                          SliverFillRemaining(
-                            child: Center(
-                              child: Text(
-                                showArchived ? loc.sessionListEmptyArchived : loc.sessionListEmpty,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )
-                    : ListView.builder(
-                        physics: const AlwaysScrollableScrollPhysics(),
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        itemCount: sessions.length,
-                        itemBuilder: (_, index) {
-                          final session = sessions[index];
-                          final cubit = context.read<SessionListCubit>();
-                          final isArchived = session.time?.archived != null;
-                          final activityInfo = state.activeSessionIds[session.id];
-                          return _SessionTile(
-                            projectId: cubit.projectId,
-                            session: session,
-                            isArchived: isArchived,
-                            isActive: activityInfo != null,
-                            awaitingInput: activityInfo?.awaitingInput ?? false,
-                            backgroundTaskCount: activityInfo?.backgroundTaskCount ?? 0,
-                            onLongPress: () => _showSessionActions(context: context, session: session),
-                            onSwipe: () => isArchived
-                                ? _unarchiveSession(context: context, cubit: cubit, sessionId: session.id)
-                                : _showArchiveSheet(context: context, cubit: cubit, session: session),
-                          );
-                        },
-                      ),
-              ),
-            ),
-          ],
-        ),
-        SessionListStaleProject() => _StaleProjectView(
-          onBack: () => context.pop(),
-        ),
-        SessionListFailed(:final error) => _ErrorView(
-          error: error,
-          onRetry: () => context.read<SessionListCubit>().retryLoadSessions(),
-        ),
+        );
+      },
+      onSessionLongPress: (session) => _showSessionActions(context: context, session: session),
+      onSessionSwipe: (session) {
+        final cubit = context.read<SessionListCubit>();
+        if (session.time?.archived != null) {
+          _unarchiveSession(context: context, cubit: cubit, sessionId: session.id);
+        } else {
+          _showArchiveSheet(context: context, cubit: cubit, session: session);
+        }
       },
     );
   }
