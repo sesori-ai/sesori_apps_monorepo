f2e2f8a75532c4f5424212d0de67d90774d0af10
Alexandru Dochioiu <alex@dochioiu.com>
2026-08-13 16:56:00 +0300

    Document glass scaffold compatibility wrapper

--- client/module_prego/lib/components/navigation/prego_glass_scaffold.dart
+++ client/module_prego/lib/components/navigation/prego_glass_scaffold.dart
@@ -452,6 +452,7 @@ class _PregoGlassScaffoldState() extends State<PregoGlassScaffold> {
             builder: (context, bannerHeight, _) => buildScaffold(bannerHeight),
           );
 
+    // Remove this wrapper once liquid_glass_widgets migrates from the Flutter SDK Material and Cupertino libraries to material_ui and cupertino_ui.
     return Scaffold(
       backgroundColor: Colors.transparent,
       resizeToAvoidBottomInset: false,
